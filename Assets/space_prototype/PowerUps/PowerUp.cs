81e8ba46a3a8180a7aad6fa06e73255d4fa1ec47
bmod <bmodone@gmail.com>
2017-07-29 13:49:05 -0700

    Removing crap and adding crap

---
+++ Assets/space_prototype/PowerUps/PowerUp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public enum Type
+    {
+        Health,
+        Oxygen,
+        Battery
+    }
+
+    public Type type = Type.Health;
+    public int amount = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (type)
+        {
+            case Type.Battery:
+                break;
+            case Type.Health:
+                var damageable = other.GetComponent<Damageable>();
+                if (damageable == null)
+                    return;
+                damageable.AddHealth(amount);
+                Destroy(gameObject);
+                break;
+            case Type.Oxygen:
+                break;
+        }
+    }
+}
