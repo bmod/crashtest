b75a273a246ee5ef0e347cf48c7e19ec4e0a1cde
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-07-29 09:02:20 -0700

    you can kill your astronaut

---
+++ Assets/AstronautDeathEffects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AstronautDeathEffects : MonoBehaviour {
+
+
+	public Damageable astronautDamageable;
+
+	// Use this for initialization
+	void Start () {
+		astronautDamageable.OnKilled += GameOverOnKilled;
+	}
+
+	void GameOverOnKilled(Missile source) {
+		Debug.Log ("Game over mon!");
+		Destroy (astronautDamageable.gameObject);
+		Debug.LogError ("We need to update this function with Game over UI");
+		StartCoroutine (RestartScene ());
+	}
+
+	IEnumerator RestartScene() {
+		yield return new WaitForSeconds (2f);
+		SceneManager.LoadScene (0);
+	}
+
+	void OnDestroy() {
+		astronautDamageable.OnKilled -= GameOverOnKilled;
+	}
+
+}
---
+++ Assets/Missile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour {
+
+	public int damageInflicted = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("collided with something: " + other.name);
+		Damageable damageable = other.GetComponent<Damageable> ();
+		if (damageable != null) {
+			damageable.TakeDamage (damageInflicted, this);
+		}
+
+
+		Destroy (gameObject);
+	}
+}
