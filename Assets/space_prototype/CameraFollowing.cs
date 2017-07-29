301e586b2c5c3cb38ba178d060bacd168b71e534
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-07-29 10:34:15 -0700

    screen shake on damage

--- Assets/space_prototype/CameraFollowing.cs
+++ Assets/space_prototype/CameraFollowing.cs
@@ -7,13 +7,79 @@ public class CameraFollowing : MonoBehaviour {
 
 	public Transform player;
 
+	public Damageable playerDamageable;
+
 	// Use this for initialization
 	void Start () {
+		GameObject go = GameObject.Find ("SpaceMan");
+		if (go != null) {
 		
+
+			playerDamageable = go.GetComponent<Damageable> ();
+			playerDamageable.OnDamageTaken += ShakeOnDamaged;
+		} else {
+			Debug.LogError ("Couldn't find the SpaceMan object in camera");
+		}
+
+	}
+
+	void OnDestroy() {
+		if (playerDamageable != null) {
+			playerDamageable.OnDamageTaken -= ShakeOnDamaged;
+		}
+	}
+
+	public float shakeMagnitude = .2f;
+	void ShakeOnDamaged(int amount, Missile source) {
+		DoShake (shakeMagnitude, .5f, null);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);	
+		transform.position = new Vector3(player.position.x, player.position.y, transform.position.z) + shakingOffset;	
 	}
+
+
+
+
+	private Vector3 shakingOffset = Vector3.zero;
+
+	Coroutine lastShakeRoutine;
+	public void DoShake(float magnitude, float duration, AnimationCurve curve) {
+		if (lastShakeRoutine != null) {
+			StopCoroutine (lastShakeRoutine);
+		}
+
+		lastShakeRoutine = StartCoroutine (ShakeProcess(magnitude, duration, curve));
+
+	}
+
+	private IEnumerator ShakeProcess(float magnitude, float duration, AnimationCurve curve) {
+		if (curve == null) {
+			curve = AnimationCurve.EaseInOut (0f, 1f, 1f, 0f);
+		}
+
+
+
+		float ogOffsetPossible = magnitude / 2f;
+		float offsetPossible = ogOffsetPossible;
+		//		Debug.Log("doing a magnitude of " + magnitude + " and offset possible is " + offsetPossible);
+		float timeRunning = 0f;
+		while (timeRunning < duration) {
+			offsetPossible = ogOffsetPossible * curve.Evaluate(timeRunning / duration);
+
+			//			Debug.Log("offset possible evaled: " + offsetPossible);
+
+			shakingOffset = new Vector3 (
+				Random.Range(-offsetPossible, offsetPossible), 
+				Random.Range(-offsetPossible, offsetPossible), 
+				Random.Range(-offsetPossible, offsetPossible));
+
+			timeRunning += Time.deltaTime;
+			yield return new WaitForEndOfFrame ();
+		}
+
+		shakingOffset = Vector3.zero;
+	}
+
 }
