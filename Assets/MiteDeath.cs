438c34a175542a43a31dffd4f61d80db2b348fba
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-07-29 15:43:36 -0700

    mite behavior

---
+++ Assets/MiteDeath.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiteDeath : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Damageable> ().OnKilled += BlowUpOnKilled;
+	}
+
+	void OnDestroy() {
+		GetComponent<Damageable> ().OnKilled -= BlowUpOnKilled;
+	}
+
+	void BlowUpOnKilled(Missile source) {
+		Destroy (gameObject);
+	}
+
+}
---
+++ Assets/MiteSeeking.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiteSeeking : MonoBehaviour {
+	
+
+	public float distanceToChangeToAttacking = 2f;
+
+	public float seekPower = 2f;
+	// Update is called once per frame
+	void Update () {
+		Seek ();
+		CheckForAnimChange ();
+
+	}
+
+	bool attackingSet = false;
+
+	void CheckForAnimChange() {
+		if (InRange () && !attackingSet) {
+			attackingSet = true;
+			GetComponent<Animator> ().SetTrigger ("Attack");
+		} else if(attackingSet && !InRange()) {
+			attackingSet = false;
+			GetComponent<Animator> ().SetTrigger ("Seek");
+		}
+	}
+
+
+
+	bool InRange() {
+		bool inr = distanceToChangeToAttacking * distanceToChangeToAttacking >
+		(PlayerControls.instance.astronaut.position - transform.position).sqrMagnitude;
+		return inr;
+	}
+
+	void Seek() {
+		Vector2 directions = PlayerControls.instance.astronaut.position - transform.position;
+
+		directions = directions + PlayerControls.instance.astronaut.GetComponent<Rigidbody2D> ().velocity;
+
+		directions = directions.normalized * seekPower;
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (directions.x, directions.y));
+	}
+
+}
