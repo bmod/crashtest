8ececdb301920c26253da04e64a6760767eaca3b
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-07-29 20:20:37 -0700

    hatcheries

---
+++ Assets/HatcherySensor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatcherySensor : MonoBehaviour {
+
+
+	public MiteHatchery owner;
+	void OnTriggerEnter2D(Collider2D other) {
+
+		//		Debug.Log ("something came close to a hatchery: " + other.name);
+		if (other.CompareTag ("Player")) {
+
+			owner.playerInVicinity = true;
+		}
+	}
+
+
+	void OnTriggerExit2D(Collider2D other) {
+		//		Debug.Log ("something left proximity to hatchery: " + other.name);
+		if (other.CompareTag ("Player")) {
+
+			owner.playerInVicinity = false;
+		}
+	}
+}
