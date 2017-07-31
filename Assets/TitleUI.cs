5ba8f27acda6cac92fc45e8b0f67d33b80a0a036
Jeron Paraiso <jeronmparaiso6a@gmail.com>
2017-07-30 22:22:53 -0700

    instructions

--- Assets/TitleUI.cs
+++ Assets/TitleUI.cs
@@ -6,6 +6,9 @@ using UnityEngine.SceneManagement;
 public class TitleUI : MonoBehaviour {
 
 
+	public RectTransform instructionsWindow;
+	public RectTransform mainTitleMenu;
+
 	public void StartGame() {
 		Debug.Log ("start clicked!");
 		SceneManager.LoadScene ("SpaceMan");
@@ -15,4 +18,11 @@ public class TitleUI : MonoBehaviour {
 		Debug.Log ("quit clicked!");
 		Application.Quit ();
 	}
+
+	public void ShowInstructions() {
+		mainTitleMenu.gameObject.SetActive(false);
+		instructionsWindow.gameObject.SetActive (true);
+
+	}
+
 }
